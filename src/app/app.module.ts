16e05a1ea702142552009a0dc5ffe75c219dd8bd
mahrliox <mramirez@anivia.mx>
2019-03-10 21:40:15 -0600

    se corrigio un error de importacion de componente inexistente

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './Login/login/login.component';
+// import { LoginComponent } from './Login/login/login.component';
 import { LoginViewComponent } from './login-page/login-view/login-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
+    // LoginComponent,
     LoginViewComponent
   ],
   imports: [
